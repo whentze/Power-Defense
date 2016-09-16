2be33a6fca7acda3c617cb50e8d56f9cacc15feb
Jonas Schröder <jonas@monstadebian2>
2016-09-17 01:17:49 +0200

    removed a usleep

--- main.cpp
+++ main.cpp
@@ -90,7 +90,6 @@ int main(int argc, char *argv[]) {
     SDL_RenderPresent(renderer);
 
     gameLoop(renderer);
-    usleep(10000000);
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
