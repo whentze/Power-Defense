e08e6f733d90ea382bd93c6e654f496ce4845909
Jonas Schröder <jonas@monstadebian2>
2016-09-17 01:09:48 +0200

    corrected wrong constructors

--- colors.h
+++ colors.h
@@ -1,7 +1,7 @@
 #pragma once
 
-static const SDL_Color COLOR_WHITE  = SDL_Color(255, 255, 255);
-static const SDL_Color COLOR_BLACK  = SDL_Color(  0,   0,   0);
-static const SDL_Color COLOR_RED    = SDL_Color(139,   0,   0);
-static const SDL_Color COLOR_BLUE   = SDL_Color(  0,   0, 139);
-static const SDL_Color COLOR_GREEN  = SDL_Color(  0, 100,   0);
+static const SDL_Color COLOR_WHITE  = {255, 255, 255};
+static const SDL_Color COLOR_BLACK  = {  0,   0,   0};
+static const SDL_Color COLOR_RED    = {139,   0,   0};
+static const SDL_Color COLOR_BLUE   = {  0,   0, 139};
+static const SDL_Color COLOR_GREEN  = {  0, 100,   0};
