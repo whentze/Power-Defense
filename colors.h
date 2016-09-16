243100947a60c86dbc95ff6f92433e8f0fa564ee
Jonas Schröder <jonas@monstadebian2>
2016-09-17 00:08:35 +0200

    Add some static const SDL_Colors

---
+++ colors.h
@@ -0,0 +1,7 @@
+#pragma once
+
+static const SDL_Color COLOR_WHITE  = SDL_Color(255, 255, 255);
+static const SDL_Color COLOR_BLACK  = SDL_Color(  0,   0,   0);
+static const SDL_Color COLOR_RED    = SDL_Color(139,   0,   0);
+static const SDL_Color COLOR_BLUE   = SDL_Color(  0,   0, 139);
+static const SDL_Color COLOR_GREEN  = SDL_Color(  0, 100,   0);
