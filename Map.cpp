e4fff1d07ea772c2d5d5a2b299987a2c756feaec
Jonas Schröder <jonas@monstadebian2>
2016-09-17 03:15:05 +0200

    ragecommit

--- Map.cpp
+++ Map.cpp
@@ -1,13 +1,13 @@
 #include "Map.h"
 
-Map::Map(const std::string& filename) {
+Map::Map(const std::string &filename) {
     tmxmap = Tmx::Map();
     tmxmap.ParseFile(std::string(CMAKE_SOURCE_DIR) + filename);
     auto objectGroup = tmxmap.GetObjectGroup(0);
-    auto object      = objectGroup->GetObject(0);
-    auto polyLine    = object->GetPolyline();
+    auto object = objectGroup->GetObject(0);
+    auto polyLine = object->GetPolyline();
     path = std::vector<Point>();
-    for(int i = 0; i < polyLine->GetNumPoints(); i++){
+    for (int i = 0; i < polyLine->GetNumPoints(); i++) {
         Tmx::Point tmxpoint = polyLine->GetPoint(i);
         path.push_back(Point{tmxpoint.x, tmxpoint.y});
     }
