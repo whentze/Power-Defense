014f62794ffe7929f2077bfa3c093b2b7c7a5aa4
Jonas Schröder <jonas@monstadebian2>
2016-09-17 04:20:50 +0200

    add TILE_HEIGHT TILE_WDTH

--- config.h
+++ config.h
@@ -1,4 +1,7 @@
 #pragma once
-static const int WINDOW_WIDTH =800;
-static const int WINDOW_HEIGHT = 800;
+
+static const int WINDOW_WIDTH = 512;
+static const int WINDOW_HEIGHT = 384 ;
+static const int TILE_WIDTH = 32 ;
+static const int TILE_HEIGHT = 32 ;
 static const int FRAMES_PER_SECOND = 30;
