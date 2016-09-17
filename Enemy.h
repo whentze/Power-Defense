68bf4c89b76e3cd88a7dde9e33f08fcf99bc5134
Jonas Schröder <jonas@monstadebian2>
2016-09-17 03:13:43 +0200

    raaageee

--- Enemy.h
+++ Enemy.h
@@ -10,15 +10,17 @@
 
 class Enemy : public GameObject {
 public:
-    Enemy(Map map, int health, float speed);
+    Enemy(Map& map, int health, float speed, SDL_Renderer* renderer);
     virtual ~Enemy();
 
     virtual void update();
     void hit(Tower& source, int damage);
+    SDL_Renderer* renderer;
 
 private:
     int maxHealth;
     int health;
     float speed;
     std::vector<Point>::size_type pathIndex;
+    Map &map;
 };
