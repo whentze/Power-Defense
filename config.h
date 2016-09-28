bc17d67266cfb8b8e61da739bcdfeebcee281cef
muvlon <muvlon@muvmix>
2016-09-28 19:32:17 +0200

    sensible window size in config.h

--- config.h
+++ config.h
@@ -1,4 +1,6 @@
 #pragma once
-static const int WINDOW_WIDTH =800;
-static const int WINDOW_HEIGHT = 800;
+static const int TILE_WIDTH    = 32;
+static const int TILE_HEIGHT   = 32;
+static const int WINDOW_WIDTH  = TILE_WIDTH  * 16;
+static const int WINDOW_HEIGHT = TILE_HEIGHT * 12;
 static const int FRAMES_PER_SECOND = 30;
