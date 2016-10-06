990ba2a1c1122d0952cb793a564b0300a3b145f9
whentze <whentze@miaplaza.com>
2016-10-06 06:38:47 +0200

    fix towers being unclickable

--- mouseManager.h
+++ mouseManager.h
@@ -52,7 +52,7 @@ static void mouseHandler(const Point pos, SDL_Event &ev) {
             if (xPos <= MAP_WIDTH) {
                 Tower *actualTower = NULL;
                 for (auto *object : allGameObjects) {
-                    if ((int) object->pos.x == xPos && (int) object->pos.y == yPos) {
+                    if ((int) object->pos.x/TILE_WIDTH == xPos && (int) object->pos.y/TILE_HEIGHT == yPos) {
                         actualTower = (Tower *) object;
                         break;
                     }
