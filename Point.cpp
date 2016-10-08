2e66682311bc76cac3196e9ec202aabe20bd7b27
whentze <whentze@miaplaza.com>
2016-10-08 06:18:43 +0200

    add GridPoint struct

--- Point.cpp
+++ Point.cpp
@@ -1,5 +1,8 @@
 #include "Point.h"
 #include "util.h"
+#include "config.h"
+
+#include <math.h>
 
 void Point::moveTowards(const Point other, float length){
     float dist = distance(*this, other);
@@ -10,4 +13,12 @@ void Point::moveTowards(const Point other, float length){
         this->x += (other.x - this->x) * length/dist;
         this->y += (other.y - this->y) * length/dist;
     }
+}
+
+GridPoint Point::snap() {
+    return { (unsigned)this->x / TILE_WIDTH, (unsigned)this->y / TILE_HEIGHT };
+}
+
+Point GridPoint::center(){
+    return { (this->x + 0.5f) * TILE_WIDTH, (this->y + 0.5f) * TILE_WIDTH };
 }
--- Point.h
+++ Point.h
@@ -1,9 +1,19 @@
 #pragma once
 
+struct GridPoint;
+
 struct Point{
     float x;
     float y;
 
     // move this Point length units towards the other Point
     void moveTowards(const Point other, float length);
+    GridPoint snap();
+};
+
+struct GridPoint{
+    unsigned x;
+    unsigned y;
+
+    Point center();
 };
