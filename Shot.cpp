8c607aa5be5fbd6ca7ad71b384288dcd76f7684f
whentze <whentze@miaplaza.com>
2016-10-13 18:02:47 +0200

    speed up bullets a little

--- Shot.cpp
+++ Shot.cpp
@@ -9,7 +9,7 @@ Shot::Shot(Point pos, Tower* parent, Enemy* target, const int damage) {
 	this->pos = pos;
 	this->target = target;
 	this->parent = parent;
-	this->speed  = 3.5;
+	this->speed  = 5;
 	this->damage = damage;
 	ID = 3;
 	this->sprites.push_back(Sprite(this->pos, 16, 16, "/assets/Shot.png"));
