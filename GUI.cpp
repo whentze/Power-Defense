34d19de84aac9df83db77224b1615e84cf9ae683
whentze <whentze@miaplaza.com>
2016-11-25 05:28:47 +0100

    delete commented-out code in GUI.cpp

--- GUI.cpp
+++ GUI.cpp
@@ -68,42 +68,6 @@ void GUI::initGUI() {
     addGUIElement(path_gamestats, new Label("", {2 - MAP_WIDTH, MAP_HEIGHT + 1}, 5, 1, COLOR_WHITE, GUIFunctions::setLabelMoney));
     addGUIElement(path_gamestats, new Label("", {8 - MAP_WIDTH, MAP_HEIGHT + 1}, 5, 1, COLOR_WHITE, GUIFunctions::setLabelPoint));
 
-    //addGUIElement(GUI::paths[path_gamestats], new Label("");
-/*
-    root->getChild(paths[path_towermenu])->children.push_back(
-            new Button("Upgrade", {1, 1}, 5, 1, COLOR_GREEN, COLOR_RED, GUIFunctions::upgradeTower));
-    root->getChild(paths[path_towermenu])->children.push_back(
-            new Button("Sell", {1, 2}, 5, 1, COLOR_GREEN, COLOR_RED, GUIFunctions::sellTower));
-
-    root->getChild(paths[path_menus])->children.push_back(new GUIObject());//buyMenu
-    root->getChild(paths[path_buymenu])->children.push_back(
-            new Symbol(std::vector<std::string>{"/assets/TowerBase.png", "/assets/TowerTurret.png"}, GridPoint(8, 1)));
-    //main menu
-    root->getChild(paths[path_menus])->children.push_back(new GUIObject());
-    root->getChild(paths[path_mainmenu])->children.push_back(
-            new Button("Start", {1, 7}, 5, 1, COLOR_GREEN, COLOR_RED, GUIFunctions::startGame));
-    root->getChild(paths[path_mainmenu])->children.push_back(
-            new Button("End", {1, 8}, 5, 1, COLOR_GREEN, COLOR_RED, GUIFunctions::endGame));
-
-
-    root->children.push_back(new GUIObject()); //stats
-    root->getChild(paths[path_stats])->children.push_back(new GUIObject()); //globalstats
-
-    root->getChild(paths[path_globalstats])->children.push_back(
-            new Label("", {7, WINDOW_HEIGHT / TILE_HEIGHT - 2}, 5, 1, COLOR_WHITE,
-                      GUIFunctions::setLabelPoint));//points
-    root->getChild(paths[path_globalstats])->children.push_back(
-            new Label("Points:", {1, WINDOW_HEIGHT / TILE_HEIGHT - 2}, 5, 1, COLOR_WHITE, nullptr));
-
-    root->getChild(paths[path_globalstats])->children.push_back(
-            new Label("", {7, WINDOW_HEIGHT / TILE_HEIGHT - 1}, 5, 1, COLOR_WHITE, GUIFunctions::setLabelMoney));//money
-    root->getChild(paths[path_globalstats])->children.push_back(
-            new Label("Money:", {1, WINDOW_HEIGHT / TILE_HEIGHT - 1}, 5, 1, COLOR_WHITE, nullptr));
-
-    for (auto element: root->getChild(paths[path_globalstats])->traverse()) {
-        element->isActivated = true;
-    }
-*/
     //activate MapOverlays and MainMenu at the beginning of the Game
     for (int i = 0; i < MAP_WIDTH; i++) {
         for (int j = 0; j < MAP_HEIGHT; j++) {
